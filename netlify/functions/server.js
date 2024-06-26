895c12b50b126e41499c817e0ca0da0f0048502c
Dan Edwards <daniel.edwards96@yahoo.com>
2024-06-26 09:41:17 +0100

    Install security packages

--- netlify/functions/server.js
+++ netlify/functions/server.js
@@ -4,21 +4,23 @@ const basicAuth = require('express-basic-auth');
 const mongoose = require('mongoose');
 const router = express.Router();
 const serverless = require('serverless-http');
+const bcrypt = require('bcrypt');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 require('dotenv').config();
 
 const app = express();
 
 app.use(cors());
-
 app.use(express.json());
-app.use('/.netlify/functions/server', router);
+app.use(helmet());
 
 const MONGODB_URI = process.env.MONGODB_URI;
 const NODE_ENV = process.env.NODE_ENV;
 const PORT = process.env.PORT || 5001;
 
-console.log(`MongoDB URI: ${MONGODB_URI}`);
-console.log(`Environment: ${NODE_ENV}`);
+// console.log(`MongoDB URI: ${MONGODB_URI}`);
+// console.log(`Environment: ${NODE_ENV}`);
 
 mongoose.connect(MONGODB_URI);
 
@@ -35,19 +37,43 @@ const Film = mongoose.model('Film', filmSchema, 'top_films');
 
 router.get('/', (req, res) => {
 	res.send(`
-	  <!DOCTYPE html>
-	  <html lang="en">
-	  <head>
-		<meta charset="UTF-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<title>Server Status</title>
-	  </head>
-	  <body>
-		<h1>The server is running!</h1>
-		<p>The database connection is ${MONGODB_URI}</p>
-	  </body>
-	  </html>
-	`);
+      <!DOCTYPE html>
+      <html lang="en">
+      <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Server Status</title>
+      </head>
+      <body>
+        <h1>The server is running!</h1>
+        <p>The database connection is ${MONGODB_URI}</p>
+      </body>
+      </html>
+    `);
+});
+
+const users = {
+	Dan: bcrypt.hashSync(process.env.USER_PASSWORD, 10),
+};
+
+// Rate limiting middleware
+const limiter = rateLimit({
+	windowMs: 15 * 60 * 1000,
+	max: 100,
+	message: 'Too many requests, please try again later.',
+});
+
+app.use(limiter);
+
+router.post('/login', async (req, res) => {
+	console.log(`Request received.`);
+	const { username, password } = req.body;
+
+	if (users[username] && (await bcrypt.compare(password, users[username]))) {
+		res.json({ message: 'Login successful' });
+	} else {
+		res.status(401).json({ message: 'Invalid credentials' });
+	}
 });
 
 router.get('/films', async (req, res) => {
